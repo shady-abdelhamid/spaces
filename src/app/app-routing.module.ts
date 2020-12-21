704fa67e7400f58619f24bd2fb9ab68e057296e7
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-12-21 21:20:12 +0200

    Update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
   {
-    path: '',
+    path: 'users',
     loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
-  }
+  },
 ];
 
 @NgModule({
--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserListComponent } from './user-list/user-list.component';
 
 import { UsersComponent } from './users.component';
 
-const routes: Routes = [{
-  path: '', component: UsersComponent, children: [
-    { path: 'users', component: UserListComponent },
-  ]
-}];
+const routes: Routes = [{ path: '', component: UsersComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
