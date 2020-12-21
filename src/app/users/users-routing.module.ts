9fe34e4b2a2099eb00a87f4341ce9a699204949d
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-12-21 21:26:14 +0200

    Update route

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
 
 import { UsersComponent } from './users.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+const routes: Routes = [{
+  path: '', component: UsersComponent, children: [
+    { path: '', component: UserListComponent },
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
