813f64e451b4dd850a400de0d30347f3cfb66ab3
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-12-21 23:55:10 +0200

    Add reactive forms

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  modalHidden = true;
+  addUserForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.addUserForm = new FormGroup({
+      name: new FormControl(''),
+      job: new FormControl(''),
+    });
+  }
+
+  public closeModal(): void {
+    this.modalHidden = true;
   }
 
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -7,6 +7,7 @@ import { UserListComponent } from './user-list/user-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UsersService } from './users.service';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,6 +15,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     UsersRoutingModule
   ],
