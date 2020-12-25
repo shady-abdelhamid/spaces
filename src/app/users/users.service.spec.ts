f2b5bdd22f3f06e340efd6ab6db253f0fb0386f8
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-12-25 19:43:53 +0200

    Update linting

--- src/app/users/users.service.spec.ts
+++ src/app/users/users.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UsersService } from './users.service';
 import { HttpClient } from '@angular/common/http';
 
