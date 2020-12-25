88e02661bf91eac6e2956516a1417dda3a8f2fd3
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-12-25 19:33:17 +0200

    Add update user scenario

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Cta } from 'src/app/shared/modal/cta.enum';
+import { User } from '../user.model';
 import { UsersService } from '../users.service';
 
 @Component({
@@ -8,9 +11,19 @@ import { UsersService } from '../users.service';
 })
 export class UserListComponent implements OnInit {
   public users = [];
+
+  modalHidden = true;
+  editUserForm: FormGroup;
+  selectedUserId: number;
+
   constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
+    this.editUserForm = new FormGroup({
+      name: new FormControl(''),
+      job: new FormControl(''),
+    });
+
     this.usersService.getUsers().subscribe({
       next: (response: any) => {
         this.users = response;
@@ -20,6 +33,24 @@ export class UserListComponent implements OnInit {
       },
       complete: () => { }
     });
+  }
+
+  /**
+   * fills user form with datga before accessing edit user modal
+   * @param user user to be filled before accessing edit user modal
+   */
+  public fillUserForm(user: User): void {
+    const { id, name, job } = user;
+    this.selectedUserId = id;
+    this.editUserForm.get('name').setValue(name);
+    this.editUserForm.get('job').setValue(job);
+  }
+
+  public onModalCtaClick(cta: Cta): void {
+    if (cta === Cta.Submit) {
+      const { name, job } = this.editUserForm.value;
+      this.usersService.updateUser(this.selectedUserId, name, job).subscribe(_ => { });
+    }
   }
 
 }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -26,11 +26,18 @@ export class UsersService {
     );
   }
 
-
   public createUser(name: string, job: string): Observable<User> {
     const payload = { name, job };
 
     return this.http.post(this.baseRoute, payload).pipe(
+      map((value) => new User(value))
+    );
+  }
+
+  public updateUser(id: number, name: string, job: string): Observable<User> {
+    const payload = { name, job };
+
+    return this.http.put(`${this.baseRoute}/${id}`, payload).pipe(
       map((value) => new User(value))
     );
   }
