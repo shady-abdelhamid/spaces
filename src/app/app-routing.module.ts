4635e616389435d05148e190fcc3d5efa12b40f0
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-12-26 07:32:31 +0200

    Add login page template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login', component: LoginComponent
+  },
   {
     path: 'users',
     loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
