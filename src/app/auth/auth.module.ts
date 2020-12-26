181df788876da010a3bae8b8d050b30bdd3762ce
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-12-26 18:32:25 +0200

    Authenticate and redirect to users

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('eve.holt@reqres.in'),
+      password: new FormControl('cityslicka'),
+    });
+  }
+
+  public login(): void {
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login(email, password).subscribe((_) => {
+      this.router.navigate(['users']);
+    });
   }
 
 }
